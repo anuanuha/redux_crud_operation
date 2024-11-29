a3ca602d015412267b4bc07eb22146874cd1e9b4
Anu H A <anu.ha@cigniti.com>
2024-11-29 13:25:35 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useSelector, useDispatch} from "react-redux"
+import { addUser , deleteUser, updateUser} from './feature/Users';
+import { useState } from 'react';
 function App() {
+  const userList = useSelector((state)=>state.users.value);
+  const dispatch = useDispatch();
+  const [name,setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [newUsername, setNewUsername]= useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='adduser'>
+      <input type='text' placeholder='name' onChange={(event)=>{setName(event.target.value)}}></input>
+      <input type='text' placeholder='username' onChange={(event)=>{setUsername(event.target.value)}}></input>
+      <button onClick={()=>{dispatch(addUser({id:userList[userList.length-1].id+1,name,username}))}}>add user</button>
+      </div>
+      <div  className='displayuser'>
+        {userList.map((user)=>{
+         return <div>
+          <h1>{user.name}</h1>
+         <h1>{user.username}</h1>
+         <input type='text' placeholder='new username...' onChange={(event)=>{setNewUsername(event.target.value)}}></input>
+         <button onClick={()=>{dispatch(updateUser({id:user.id, username:newUsername}))}}>Update Username</button>
+         <button onClick={()=>{dispatch(deleteUser({id:user.id}))}}>Delete User</button>
+         </div>
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/feature/FakeData.js
@@ -0,0 +1,32 @@
+export const UserData =[
+    {
+        id:1,
+        name:"anu",
+        username:"anu@gmail.com"
+    },
+    {
+        id: 2,
+        name: "john",
+        username: "john@gmail.com"
+      },
+      {
+        id: 3,
+        name: "emma",
+        username: "emma@gmail.com"
+      },
+      {
+        id: 4,
+        name: "liam",
+        username: "liam@gmail.com"
+      },
+      {
+        id: 5,
+        name: "olivia",
+        username: "olivia@gmail.com"
+      },
+      {
+        id: 6,
+        name: "noah",
+        username:"noah@gmail.com"
+      }
+]
